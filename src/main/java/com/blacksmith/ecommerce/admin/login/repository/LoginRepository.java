2ce7713ef6907d4b5d1f63aed672f339ae830344
wjstodsss <wjstodsss@naver.com>
2024-03-18 17:54:40 +0900

    초기화

--- src/main/java/com/blacksmith/ecommerce/admin/login/repository/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.blacksmith.ecommerce.admin.login.repository;
-
-import com.blacksmith.ecommerce.admin.login.domain.MemberDTO;
-
-public interface LoginRepository {
-
-	public MemberDTO login(MemberDTO memberDTO);
-
-}
--- src/main/java/com/blacksmith/ecommerce/admin/login/service/LoginService.java
+++
@@ -1,7 +0,0 @@
-package com.blacksmith.ecommerce.admin.login.service;
-
-import com.blacksmith.ecommerce.admin.login.domain.MemberDTO;
-
-public interface LoginService {
-	public boolean login(MemberDTO memberDTO);
-}
